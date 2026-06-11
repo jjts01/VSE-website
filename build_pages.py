82ad0f72647220bf2495e5dc11a8872a5b098d9e
James Jones <jj@jjtechservices.co.uk>
2026-06-11 08:25:33 +0000

    Brand typeface: Milliard webfonts replace Archivo

--- build_pages.py
+++ build_pages.py
@@ -34,7 +34,7 @@ def page(slug, title, desc, hero_kicker, hero_h1, hero_lede, body):
 <meta property="og:image" content="https://www.virtualstudio.events/assets/img/hero-manchester.jpg">
 <link rel="preconnect" href="https://fonts.googleapis.com">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-<link href="https://fonts.googleapis.com/css2?family=Archivo:ital,wght@0,400;0,500;0,600;1,400&family=Archivo+Expanded:wght@600;700;800&family=Instrument+Serif:ital@1&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Instrument+Serif:ital@1&display=swap" rel="stylesheet">
 <link rel="stylesheet" href="assets/css/main.css">
 </head>
 <body>
